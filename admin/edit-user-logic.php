a3e4d3a18c0713044dc33b6167f2f37b8d8211ed
ykkt85 <kato.yuka.td@alumni.tsukuba.ac.jp>
2023-02-28 15:52:22 +0900

    fix: 管理者以外（投稿者）とログインユーザーがedit-user-logic.phpへアクセスできるバグを修正

--- admin/edit-user-logic.php
+++ admin/edit-user-logic.php
@@ -6,6 +6,14 @@ if (isset($_POST['submit'])){
     $userID = filter_var($_POST['user_ID'], FILTER_SANITIZE_NUMBER_INT);
     $roleID = filter_var($_POST['role_ID'], FILTER_SANITIZE_NUMBER_INT);
 
+    // 管理者以外（投稿者）がアクセスした場合、または
+    // ログインユーザーが本人のedit-user-logic.phpにアクセスしようとした場合
+    if ($_SESSION['role_ID'] === 0 || $_SESSION['user_ID'] === $userID){
+        $_SESSION['nonadmin_error'] = 'アクセス権限がありません';
+        header('location: ' . ROOT_URL . 'message.php');
+        die();
+    }
+
     // DBの値を上書き保存
     $connection = dbconnect();
     $stmt = $connection->prepare('UPDATE users SET role_ID=?, updated_at=CURRENT_TIMESTAMP() WHERE user_ID=? LIMIT 1');
