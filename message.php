3444f033a18210ac38a547501bfa9104361d3784
ykkt85 <kato.yuka.td@alumni.tsukuba.ac.jp>
2023-02-28 16:31:03 +0900

    feat: message.phpにセッション値のメッセージを追加

--- message.php
+++ message.php
@@ -5,23 +5,29 @@ include __DIR__ . '/partials/header.php';
 <!--================================ HTML ================================-->
 
     <section class=" section__extra-margin">
-        <!-- パスワード変更を完了できなかった場合 -->
-        <?php if (isset($_SESSION['new_password_error'])): ?>
-            <div class ="alert__message error lg">
-                <p><?php echo $_SESSION['new_password_error'];
-                unset($_SESSION['new_password_error']) ?></p>
-            </div>
         <!-- パスワード変更を完了できた場合 -->
-        <?php elseif (isset($_SESSION['new_password_success'])): ?>
+        <?php if (isset($_SESSION['new_password_success'])): ?>
             <div class ="alert__message success lg">
                 <p><?php echo $_SESSION['new_password_success'];
                 unset($_SESSION['new_password_success']) ?></p>
+            </div>
+        <!-- パスワード変更を完了できなかった場合 -->
+        <?php elseif (isset($_SESSION['new_password_error'])): ?>
+            <div class ="alert__message error lg">
+                <p><?php echo $_SESSION['new_password_error'];
+                unset($_SESSION['new_password_error']) ?></p>
             </div>
         <!-- パスワード変更後メールが送信された場合 -->
         <?php elseif (isset($_SESSION['reset_password_success'])): ?>
             <div class ="alert__message success lg">
                 <p><?php echo $_SESSION['reset_password_success'];
                 unset($_SESSION['reset_password_success']) ?></p>
+            </div>
+        <!-- 管理者以外がユーザー編集を試みた場合 -->
+        <?php elseif (isset($_SESSION['nonadmin_error'])): ?>
+            <div class ="alert__message error lg">
+                <p><?php echo $_SESSION['nonadmin_error'];
+                unset($_SESSION['nonadmin_error']) ?></p>
             </div>
         <!-- 問い合わせフォームから問い合わせを送った場合 -->
         <?php elseif (isset($_SESSION['contact_success'])): ?>
