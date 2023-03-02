ef84831d101c6a8935aa3881b6cdd72739d374d2
ykkt85 <kato.yuka.td@alumni.tsukuba.ac.jp>
2023-03-02 19:35:02 +0900

    feat: message.phpにセッション値のメッセージを追加

--- message.php
+++ message.php
@@ -17,6 +17,12 @@ include __DIR__ . '/partials/header.php';
                 <p><?php echo $_SESSION['nonadmin_error'];
                 unset($_SESSION['nonadmin_error']) ?></p>
             </div>
+        <!-- 新しいパスワードを登録できなかった場合 -->
+        <?php elseif (isset($_SESSION['new_password_error'])): ?>
+            <div class ="alert__message error lg">
+                <p><?php echo $_SESSION['new_password_error'];
+                unset($_SESSION['new_password_error']) ?></p>
+            </div>
         <!-- 問い合わせフォームから問い合わせを送った場合 -->
         <?php elseif (isset($_SESSION['contact_success'])): ?>
             <div class ="alert__message success lg">
