cc4ec2071810c0c0958829956fa5ab88264cb5db
ykkt85 <kato.yuka.td@alumni.tsukuba.ac.jp>
2023-06-18 23:02:38 +0900

    chore: ROOT_URLの書き換え

--- config/constants.php
+++ config/constants.php
@@ -6,7 +6,7 @@ require __DIR__ . '/../vendor/autoload.php';
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
 
-define('ROOT_URL', 'http://localhost:8888/TsukubaUniversityNews/');
+define('ROOT_URL', 'https://news-php-blog.herokuapp.com/');
 define('DB_HOST', $_ENV['DB_HOST']);
 define('DB_USER', $_ENV['DB_USER']);
 define('DB_PASS', $_ENV['DB_PASS']);
