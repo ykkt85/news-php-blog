90cf6f76c46f8da697ec9009b24711d7b0eadb94
ykkt85 <kato.yuka.td@alumni.tsukuba.ac.jp>
2023-06-19 22:59:25 +0900

    chore: 開発環境用URL削除

--- config/constants.php
+++ config/constants.php
@@ -6,7 +6,6 @@ require __DIR__ . '/../vendor/autoload.php';
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
 
-# define('ROOT_URL', 'http://localhost:8888/TsukubaUniversityNews/'); ローカル開発用
 define('ROOT_URL', 'https://news-php-blog.herokuapp.com/');
 define('DB_HOST', $_ENV['DB_HOST']);
 define('DB_USER', $_ENV['DB_USER']);
